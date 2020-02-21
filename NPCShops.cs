11207a24b0a58b54c84ddd33b63e93cdb5b79af3
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-02-20 18:56:46 -0800

    Changed "Mod.ItemType" to "ModContent.ItemType"

--- NPCShops.cs
+++ NPCShops.cs
@@ -11,37 +11,37 @@ namespace jimmysmod
         {
             if (type == NPCID.ArmsDealer)  // always available
             {
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.LewisGun>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.LewisGun>());
                 nextSlot++;
 
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.SMG>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.SMG>());
                 nextSlot++;
 
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Ammo.FMJ>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.Ammo.FMJ>());
                 nextSlot++;
             }
             if (type == NPCID.ArmsDealer && NPC.downedBoss3) // after skeletron
             {
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Enfield>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.Enfield>());
                 nextSlot++;
 
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.ATRifle>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.ATRifle>());
                 nextSlot++;
             }
             if (type == NPCID.ArmsDealer && Main.hardMode) // hardmode
             {
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Ammo.DepletedUranium>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.Ammo.DepletedUranium>());
                 nextSlot++;
             }
             if (type == NPCID.ArmsDealer && NPC.downedPlantBoss) // after plantera
             {
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.M1919Shorty>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.M1919Shorty>());
                 nextSlot++;
             }
             if (type == NPCID.ArmsDealer && NPC.downedGolemBoss) // after golem
             {
 
-                shop.item[nextSlot].SetDefaults(mod.ItemType<Items.SCAR>());
+                shop.item[nextSlot].SetDefaults(ModContent.ItemType<Items.SCAR>());
                 nextSlot++;
             }
         }
