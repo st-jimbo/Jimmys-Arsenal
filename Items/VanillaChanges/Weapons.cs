48a405cbc0212703831c3095f3c654a4bf97a4f0
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-04-16 19:51:51 -0700

    Creation

---
+++ Items/VanillaChanges/Weapons.cs
@@ -0,0 +1,38 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace jimmysmod.Items.VanillaChanges
+{
+    public class Weapons : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            // Chlorophyte Claymore
+            if (item.type == ItemID.ChlorophyteClaymore)
+            {
+                item.damage = 105; // From 75
+                item.autoReuse = true; // From false
+            }
+
+            // Chlorophyte Saber
+            if (item.type == ItemID.ChlorophyteSaber)
+            {
+                item.damage = 60; // From 48
+            }
+
+            // Chlorophyte Partisan
+            if (item.type == ItemID.ChlorophytePartisan)
+            {
+                item.damage = 58; // From 49
+            }
+
+            // Brand of the Inferno
+            if (item.type == ItemID.DD2SquireDemonSword)
+            {
+                item.damage = 60; // From 44
+            }
+        }
+    }
+}
