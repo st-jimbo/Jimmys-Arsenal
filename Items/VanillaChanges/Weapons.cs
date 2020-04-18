3eafb4bd016c695571252103727f89f1c7fcc613
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-04-17 20:29:05 -0700

    Rearranged

--- Items/VanillaChanges/Weapons.cs
+++ Items/VanillaChanges/Weapons.cs
@@ -9,25 +9,6 @@ namespace jimmysmod.Items.VanillaChanges
     {
         public override void SetDefaults(Item item)
         {
-            // Chlorophyte Claymore
-            if (item.type == ItemID.ChlorophyteClaymore)
-            {
-                item.damage = 105; // From 75
-                item.autoReuse = true; // From false
-            }
-
-            // Chlorophyte Saber
-            if (item.type == ItemID.ChlorophyteSaber)
-            {
-                item.damage = 60; // From 48
-            }
-
-            // Chlorophyte Partisan
-            if (item.type == ItemID.ChlorophytePartisan)
-            {
-                item.damage = 58; // From 49
-            }
-
             // Brand of the Inferno
             if (item.type == ItemID.DD2SquireDemonSword)
             {
