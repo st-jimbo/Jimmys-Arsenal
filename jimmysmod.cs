da1893dee56f554218d67dd008102731dc3d38e1
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-04-24 05:09:58 -0700

    Recipe group and updated stuff

--- jimmysmod.cs
+++ jimmysmod.cs
@@ -1,4 +1,7 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 
 namespace jimmysmod
 {
@@ -6,6 +9,23 @@ namespace jimmysmod
 	{
 		public jimmysmod()
 		{
+		}
+
+		public override void AddRecipeGroups()
+		{
+			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Adamantite Bar", new int[]
+			{
+				ItemID.AdamantiteBar,
+				ItemID.TitaniumBar
+			});
+			RecipeGroup.RegisterGroup("jimmysmod:Adamantite", group);
+
+			RecipeGroup group2 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Mythril Bar", new int[]
+			{
+				ItemID.MythrilBar,
+				ItemID.OrichalcumBar
+			});
+			RecipeGroup.RegisterGroup("jimmysmod:Mythril", group2);
 		}
 	}
 }
