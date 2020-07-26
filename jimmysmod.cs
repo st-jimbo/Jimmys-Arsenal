1d3f67a82d4f0a80750317b532b51d0dc803216e
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-07-25 20:07:56 -0700

    Added Copper bar group

--- jimmysmod.cs
+++ jimmysmod.cs
@@ -13,17 +13,24 @@ namespace jimmysmod
 
 		public override void AddRecipeGroups()
 		{
-			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Adamantite Bar", new int[]
+			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Any Adamantite Bar", new int[]
 			{
 				ItemID.AdamantiteBar,
 				ItemID.TitaniumBar
 			});
 			RecipeGroup.RegisterGroup("jimmysmod:Adamantite", group);
 
-			RecipeGroup group2 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Mythril Bar", new int[]
+			RecipeGroup group2 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Any Mythril Bar", new int[]
 			{
 				ItemID.MythrilBar,
 				ItemID.OrichalcumBar
+			});
+			RecipeGroup.RegisterGroup("jimmysmod:Mythril", group2);
+
+			RecipeGroup group3 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Any Copper Bar", new int[]
+			{
+				ItemID.CopperBar,
+				ItemID.TinBar
 			});
 			RecipeGroup.RegisterGroup("jimmysmod:Mythril", group2);
 		}
