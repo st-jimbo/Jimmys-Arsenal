5ec674cc9bde398577fd93a1e9bb6064db8eec0c
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-07-25 19:29:28 -0700

    Added

---
+++ Items/Helltaker.cs
@@ -0,0 +1,52 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace jimmysmod.Items
+{
+	public class Helltaker : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Helltaker");
+			Tooltip.SetDefault("And henceforth you were known as the Helltaker\nUses gel for ammo");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 14;
+			item.ranged = true;
+			item.width = 62;
+			item.height = 20;
+            item.useTime = 30;
+			item.useAnimation = 6;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.knockBack = 1.5f;
+			item.value = Item.sellPrice(silver: 54);
+			item.rare = ItemRarityID.Orange;
+            item.UseSound = SoundID.Item34;
+            item.autoReuse = true;
+			item.shoot = ProjectileID.Flames;
+			item.shootSpeed = 7f;
+			item.useAmmo = AmmoID.Gel;
+		}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HellstoneBar, 12);
+            recipe.AddIngredient(ItemID.IllegalGunParts, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+		
+		// Change hold position
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-2, 1);
+		}
+	}
+}
