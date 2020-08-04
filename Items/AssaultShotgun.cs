54e642ee154621ca64030837725bc78af84c8e83
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-08-04 07:12:10 -0700

    Creation

---
+++ Items/AssaultShotgun.cs
@@ -0,0 +1,103 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace jimmysmod.Items
+{
+    public class AssaultShotgun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Assault Shotgun");
+            Tooltip.SetDefault("Redefining close quarters combat");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 32;
+            item.ranged = true;
+            item.width = 94;
+            item.height = 36;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noMelee = true;
+            item.knockBack = 6f;
+            item.value = Item.sellPrice(gold: 25);
+            item.rare = 8;
+            item.UseSound = SoundID.Item38;
+            item.autoReuse = true;
+            item.shoot = 10;
+            item.shootSpeed = 7f;
+            item.useAmmo = AmmoID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("TrenchSweeper"));
+            recipe.AddIngredient(ItemID.ShroomiteBar, 14);
+            recipe.AddIngredient(ItemID.SpectreBar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            // alt stats
+            if (player.altFunctionUse == 2)
+            {
+                item.useTime = 38;
+                item.useAnimation = 38;
+                item.UseSound = SoundID.Item36;
+            }
+            // normal stats
+            else
+            {
+                item.useTime = 20;
+                item.useAnimation = 20;
+                item.UseSound = SoundID.Item38;
+            }
+            return base.CanUseItem(player);
+        }
+
+        // Shotgun
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            // alt
+            if (player.altFunctionUse == 2)
+            {
+                int numberProjectiles = 6; // amount of shots
+                for (int i = 0; i < numberProjectiles; i++)
+                {
+                    Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(2));
+                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+                }
+            }
+            // normal
+            else
+            {
+                int numberProjectiles = 6; // amount of shots
+                for (int i = 0; i < numberProjectiles; i++)
+                {
+                    Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(8));
+                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+                }
+            }
+            return false;
+        }
+
+        // Change hold position
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-10, 1);
+        }
+    }
+}
