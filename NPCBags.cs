c56ad9659dbf5460fcffdbea588223664c10b5d7
St. Jimmy <titaniumcobaltspork@gmail.com>
2020-08-08 07:10:21 -0700

    bag

---
+++ NPCBags.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace jimmysmod
+{
+    public class NPCBags : GlobalItem
+    {
+        public override void OpenVanillaBag(string context, Player player, int arg)
+        {
+            if (context == "bossbag" && arg == ItemID.PlanteraBossBag)
+            {
+                player.QuickSpawnItem(mod.ItemType("M1919Shorty"));
+            }
+        }
+    }
+}
